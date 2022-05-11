0c06e7fce9f7ce2beac91a9d8d11efe346609f4a
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-05-11 16:12:24 -0300

    mudando home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>@yield('title') - App</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Cadastro de usuários</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Listagem de usuários</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Consulta de usuários</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Pesquisa" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Pesquisa</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    
-    <div class="app">
-        @yield('content')
-    </div>
-    
-</body>
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>@yield('title') - Bem vindos!</title>
+  </head>
+  <body>
+
+    @yield('content')
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,45 +4,55 @@
 
     @section('content')
 
-    <h1 class="text-center mt-4">Mundo animal</h1>
-<div class="container">
-    <div class="row mt-5">
-        <div class="col">
-            <img src="img/animal1.jpeg" alt="Carro">
-            <h3 class="text-center">Tucano</h3>
-            <p class="mt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus iusto aperiam ipsa illo nihil at quaerat, magni alias recusandae, eum error cupiditate nam quis facere nostrum vitae deleniti voluptatibus.</p>
+    <div class="container">
+        <h1 class="mt-5 mb-4">Lista de Produtos</h1>
+
+        <div class="row">
+            <div class="col">
+                <img class="img" src="img/maquina.jpg" alt="">
+                <h6 class="mt-2">Máquina de Cortar Cabelo</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/maquina-acabamento.jpeg" alt="">
+                <h6 class="mt-2">Máquina de acabamento</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/fonte-chaveada.png" alt="">
+                <h6 class="mt-2">Fonte chaveada</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/lanterna-cabeca.png" alt="">
+                <h6 class="mt-2">Lanterna de Cabeça</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/maquina.jpg" alt="">
+                <h6 class="mt-2">Máquina de Cortar Cabelo</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/maquina.jpg" alt="">
+                <h6 class="mt-2">Máquina de Cortar Cabelo</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/maquina.jpg" alt="">
+                <h6 class="mt-2">Máquina de Cortar Cabelo</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
+            <div class="col">
+                <img class="img" src="img/maquina.jpg" alt="">
+                <h6 class="mt-2">Máquina de Cortar Cabelo</h6>
+                <button class="btn btn-primary mb-3">Pedir</button>
+            </div>
         </div>
 
-        <div class="col">
-            <img src="img/animal2.jpeg" alt="Carro">
-            <h3 class="text-center">Cachorro</h3>
-            <p class="mt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus iusto aperiam ipsa illo nihil at quaerat, magni alias recusandae, eum error cupiditate nam quis facere nostrum vitae deleniti voluptatibus.</p>
-        </div>
 
-        <div class="col">            
-            <img src="img/animal3.jpeg" alt="Carro">
-            <h3 class="text-center">Elefante</h3>
-            <p class="mt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus iusto aperiam ipsa illo nihil at quaerat, magni alias recusandae, eum error cupiditate nam quis facere nostrum vitae deleniti voluptatibus.</p>
-        </div>
-
-        <div class="col">
-            <img src="img/cobra.jpeg" alt="Carro">
-            <h3 class="text-center">Cobra</h3>
-            <p class="mt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus iusto aperiam ipsa illo nihil at quaerat, magni alias recusandae, eum error cupiditate nam quis facere nostrum vitae deleniti voluptatibus.</p>
-        </div>
-
-        <div class="col">
-            <img src="img/coelho.jpeg" alt="Carro">
-            <h3 class="text-center">Coelho</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus iusto aperiam ipsa illo nihil at quaerat, magni alias recusandae, eum error cupiditate nam quis facere nostrum vitae deleniti voluptatibus.</p>
-        </div>
-
-        <div class="col">
-            <img src="img/coruja.jpeg" alt="Carro">
-            <h3 class="text-center">Coruja</h3>
-            <p class="mt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus iusto aperiam ipsa illo nihil at quaerat, magni alias recusandae, eum error cupiditate nam quis facere nostrum vitae deleniti voluptatibus.</p>
-        </div>
     </div>
-</div>
 
-@endsection
+    
+    
+    @endsection
